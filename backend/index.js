bff821d633c580ffed0b3c9219eeba6a70f8038f
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-17 16:21:59 +0100

    added bcrypt to hash password

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const helmet = require('helmet')
+const dotenv = require('dotenv')
+const  morgan = require('morgan')
+const userRoutes = require('./routes/userRoutes')
+const authRoutes = require('./routes/authRoutes')
+
+dotenv.config()
+
+
+//connecting to mongodb
+const connectToMongoDb = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true})
+        console.log('Connected to db')
+        app.listen(process.env.PORT || 4000,() =>{
+            console.log('server ready on port ',process.env.PORT || 4000)
+        })
+    }catch(err){
+        console.log('Error connecting to db',err)
+    }
+}
+(async () => {
+    await connectToMongoDb();
+  })();
+
+
+//middlewares
+app.use(express.json());
+app.use(morgan('common'));
+app.use(helmet());
+
+app.use('/api/users',userRoutes)
+app.use('/api/auth',authRoutes)
