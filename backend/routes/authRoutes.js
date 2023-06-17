0b46d077da2796c132e0c7597c26d099e1720426
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-17 16:21:26 +0100

    aded bcrypt to hash password

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt')
+
+//register
+router.post('/register', async (req,res) => {
+    //encypt password with bcrypt hash
+    const salt = await bcrypt.genSalt(10)
+    const encryptedPassword = await bcrypt.hash(req.bosy.password,salt)
+
+    try{
+        //create new user
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:encryptedPassword
+        })
+
+        //save user to db and send response
+        const user = await newUser.save()
+        res.status(200).json(user)
+    }catch(err){
+        console.log(err)
+    }
+
+})
+
+module.exports = router
