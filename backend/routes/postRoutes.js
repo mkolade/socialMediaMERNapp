e5b19cc983e2f8262ad33a5e53d485d169af00f8
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-18 18:11:47 +0100

    ok

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -44,7 +44,23 @@ router.delete('/:id',async (req,res) =>{
     
 })
 
-//like a post
+//like / dislike a post
+router.put('/:id/like',async (req,res)=>{
+    try{
+        const post =await Post.findById(req.params.id)
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}})
+            res.status(200).json('The post has been liked')
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}})
+            res.status(200).json('The post has been unliked')
+        }
+    }catch(err){
+        return res.status(500).json(err)
+    }
+    
+})
+
 //get a post
 //get timeline posts
 
