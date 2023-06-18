260f1677600b7bd9e6c04e9b1f30236e02c058a8
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-18 02:07:35 +0100

    fixed error in delete controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -31,7 +31,7 @@ router.put("/:id",async (req,res) =>{
 router.delete("/:id",async (req,res) =>{
     if(req.body.userId === req.params.id || req.body.isAdmin){
         try{
-            const user = await User.deleteOne(req.params.id)
+            const user = await User.findByIdAndDelete(req.params.id)
             res.status(200).json('account successfully deleted')
         }catch(err){
             res.status(500).json(err)
