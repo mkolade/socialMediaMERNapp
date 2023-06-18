cc4968a2ec138322352e012a50b0232d53ea9c4a
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-18 15:59:31 +0100

    made the post controller to create posts

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router()
 const Post = require('../models/postModel')
 
-router.get('/',(req,res) =>{
-    console.log('Post page')
+//create a post
+router.post('/', async (req,res) =>{
+    const newPost = new Post(req.body);
+    try{
+        const savedPost = await newPost.save()
+        res.status(200).json(savedPost)
+    }catch(err){
+        res.status(500).json(err)
+    }
 })
+//update a post
+//delete a post
+//like a post
+//get a post
+//get timeline posts
 
 module.exports = router
