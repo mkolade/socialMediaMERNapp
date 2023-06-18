82847ac8509f2edc66cc3a716bfd618c083c8454
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-18 02:03:29 +0100

    added delete and update controllers for the api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -20,9 +20,26 @@ router.post('/register', async (req,res) => {
         const user = await newUser.save()
         res.status(200).json(user)
     }catch(err){
-        console.log(err)
+        res.status(500).json(err)
     }
 
+})
+
+//login
+router.post('/login', async (req,res) =>{
+    try{
+        //validate user email
+        const user = await User.findOne({email:req.body.email})
+        !user && res.status(404).json("user not found")
+        //validate user password
+        const validPassword = await bcrypt.compare(req.body.password,user.password)
+        !validPassword && res.status(400).json("wrong password")
+
+        //if all validation checks is complete
+        res.status(200).json(user)
+    }catch(err){
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,47 @@
 const router = require('express').Router()
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt')
 
-router.get('/',(req,res) =>{
-    res.status(200).send('Hey its your users')
+//update user
+router.put("/:id",async (req,res) =>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(req.body.password,salt)
+            }catch(err){
+                res.status(500).json(err)
+            }
+        }
+
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id,{
+                $set:req.body
+            })
+            res.status(200).json('account successfully updated')
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        return res.status(403).json("You can only update your account")
+    }
 })
+
+//delete user
+router.delete("/:id",async (req,res) =>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            const user = await User.deleteOne(req.params.id)
+            res.status(200).json('account successfully deleted')
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        return res.status(403).json("You can only delete your account")
+    }
+})
+//get a user
+//follow a user
+//unfollow a user
 
 module.exports = router
