65e5a24cca482dbf571e5d2fd9ecca68b72bb765
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-18 02:47:43 +0100

    created the follow uuser controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -48,12 +48,35 @@ router.get("/:id",async (req,res) =>{
         if(!user){
             res.status(404).json("user not found")
         }
-        res.status(200).json(user)
+        const {password,updatedAt,createdAt,...others} = user._doc
+        res.status(200).json(others)
     }catch(err){
         res.status(500).json(err)
     }
 })
 //follow a user
+router.put("/:id/follow",async (req,res) =>{
+    if(req.body.userId !== req.params.id){
+        try{
+            const currentUser = await User.findById(req.body.userId)
+            const userToFollow = await User.findById(req.params.id)
+
+            if(!userToFollow.followers.includes(req.body.userId)){
+                await userToFollow.updateOne({$push: {followers:req.body.userId}})
+                await currentUser.updateOne({$push: {following:req.params.id}})
+
+                res.status(200).json("User has been successfully followed")
+            }else{
+                res.status(403).json("You alrady follow this user")
+            }
+
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(404).json("You cant follow yourself")
+    }
+})
 //unfollow a user
 
 module.exports = router
