04702e069f423866743d81e81a2e1fd747893d21
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-21 17:48:04 +0100

    created the base for the share component

--- frontend/src/components/Share.jsx
+++ frontend/src/components/Share.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import {PermMedia} from '@mui/icons-material'
 export default function Share() {
   return (
-    <div>
-      Hello this is share
+    <div className='share'>
+      <div className="shareWrapper">
+        <div className="shareTop">
+            {/* <img src="/src/assets/person/1.jpeg" alt="" className='shareProfileImg'/> */}
+            <input placeholder='What`s on your mind' className='shareInput' />
+        </div>
+        <hr className='shareHr'/>
+        <div className="shareBottom">
+            <div className="shareOptions">
+                <div className="shareOption">
+                    <PermMedia className='shareOptionIcon'/>
+                    <span className='shareOptionText'>Photo or video</span>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
