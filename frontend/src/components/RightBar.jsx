8cd3e32bb6a34dbae620e6cd6bd6c38fb9e1dab2
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-22 17:00:08 +0100

    designed the birthday section for rightbar

--- frontend/src/components/RightBar.jsx
+++ frontend/src/components/RightBar.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function RightBar() {
   return (
     <div className='rightbar'>
-      RightBar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/src/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Chelsea Scott</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
