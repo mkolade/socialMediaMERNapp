fde6dcb40b72f4327eef97ed0a40998b89b07fc0
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-23 23:09:11 +0100

    one stying login inputs

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -8,9 +8,13 @@ export default function Login() {
             <div className="loginDesc">Connect with friends and family all over the globe on Mksocial</div>
         </div>
         <div className="loginRight">
-            <div className="loginBox">
-                jhsdgfhkbdsbvj
-            </div>
+            <form className="loginForm">
+                <input placeholder="Email" type='email' className="loginInput" />
+                <input placeholder="Password" type='password' className="loginInput" />
+                <button className="loginSubmit" type='submit'>Log in</button>
+                <span className="loginForgot">Forgot Password?</span>
+                <button className="loginCreateNew">Create new account</button>
+            </form>
         </div>
       </div>
     </div>
