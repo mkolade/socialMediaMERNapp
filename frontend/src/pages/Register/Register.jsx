6fd7f71177508c29ff4bbd956044cdd2ddf76801
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-23 22:45:12 +0100

    added small details

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -2,8 +2,14 @@ import './Register.css'
 
 export default function Register() {
   return (
-    <div>
-      
+    <div className='login'>
+      <div className="loginWrapper">
+        <div className="loginLeft">
+            <div className="loginLogo">Mksocial</div>
+            <div className="loginDesc">Connect with friends and family all over the globe on Mksocial</div>
+        </div>
+        <div className="loginRight"></div>
+      </div>
     </div>
   )
 }
