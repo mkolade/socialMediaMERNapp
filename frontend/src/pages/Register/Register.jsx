7f4a2971e60118a477e362900cac35eb1193ff40
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-23 23:18:52 +0100

    started working on register

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -3,7 +3,23 @@ import './Register.css'
 export default function Register() {
   return (
     <div>
-        
+        <div className='register'>
+            <div className="registerWrapper">
+                <div className="registerLeft">
+                    <div className="registerLogo">Mksocial</div>
+                    <div className="registerDesc">Connect with friends and family all over the globe on Mksocial</div>
+                </div>
+                <div className="registerRight">
+                    <form className="registerForm">
+                        <input placeholder="Email" type='email' className="registerInput" />
+                        <input placeholder="Username" type='username' className="registerInput" />
+                        <input placeholder="Password" type='password' className="registerInput" />
+                        <button className="registerSubmit" type='submit'>Sign up</button>
+                        <button className="registerCreateNew">Log into account</button>
+                    </form>
+                </div>
+            </div>
+        </div>  
     </div>
   )
 }
