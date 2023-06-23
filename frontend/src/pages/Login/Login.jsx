19dda20f9d526fc16bd6e65d5b8ac9ef10aa83a9
moshoodmohammed <mohammedamoshood@gmail.com>
2023-06-23 22:45:52 +0100

    moved code to right place

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,14 @@
 import './Login.css'
 export default function Login() {
   return (
-    <div>
-      
+    <div className='login'>
+      <div className="loginWrapper">
+        <div className="loginLeft">
+            <div className="loginLogo">Mksocial</div>
+            <div className="loginDesc">Connect with friends and family all over the globe on Mksocial</div>
+        </div>
+        <div className="loginRight"></div>
+      </div>
     </div>
   )
 }
