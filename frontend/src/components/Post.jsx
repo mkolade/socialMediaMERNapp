61b7dece60e07e714c5a668b440af9f32cc06698
moshoodmohammed <mohammedamoshood@gmail.com>
2023-07-02 18:22:07 +0100

    autocapitalizd first letter of username

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -4,20 +4,23 @@ import { Users } from '../dummyData'
 import axios from 'axios'
 
 export default function Post({post}) {
+
+   //useful knowledge
   {/* ask chatgpt to explain this part indepth */}
-  const postOwner = Users.filter((user) => user.id === post.userId)[0]
+  /* const postOwner = Users.filter((user) => user.id === post.userId)[0]
+ */
   const [like,setLike] = useState(post.like)
   const [isLiked,setIsLiked] = useState(false)
   const handleLike = () =>{
     setLike(isLiked ? like - 1 : like + 1)
     setIsLiked(!isLiked)
   }
+
   const [user,setUser] = useState({})
   useEffect( () =>{
     const fetchUser = async () =>{
-      const res = await axios.get("http://localhost:8000/api/users/648f413f9ea807a0c753e5ac");
-      /* setUser(res.data) */
-      console.log(res)
+      const res = await axios.get(`http://localhost:8000/api/users/${post.userId}`);
+      setUser(res.data)
     }
     fetchUser()
   },[])
@@ -28,9 +31,13 @@ export default function Post({post}) {
         <div className="postTop">
           <div className="postTopLeft">
             {/* <img className='postProfileImg' src={postOwner.profilePicture} alt="" /> */}
-            {/* <span className="postUsername">
-              {postOwner.username}
-            </span> */}
+            <span className="postUsername">
+              {user && user.username && user.username
+                .split(' ')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(' ')
+              }
+            </span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
