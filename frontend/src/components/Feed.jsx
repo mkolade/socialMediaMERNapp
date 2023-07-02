4b7604164282fdc78e325cd2f7b6f4736c47c213
moshoodmohammed <mohammedamoshood@gmail.com>
2023-07-02 17:25:07 +0100

    fixed async error

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import Share from './Share'
 import Post from './Post'
+import axios from 'axios'
 
 export default function Feed() {
   const [posts,setPosts] = useState([])
-  useEffect(() =>{
-    console.log('fed rendered')
+
+  useEffect( () =>{
+    const fetchData = async () =>{
+      const res = await axios.get("post/timeline/all/648f413f9ea807a0c753e5ac")
+      console.log(res)
+    }
+    fetchData()
   },[])
+
   return (
     <div className='feed'>
       <div className="feedWrapper">
