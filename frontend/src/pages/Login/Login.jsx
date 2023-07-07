ed0e5a42b29735555ff42b9a58ed7771f61e3b60
moshoodmohammed <mohammedamoshood@gmail.com>
2023-07-07 13:44:25 +0100

    used useref and created context files

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,16 @@
+import { useRef } from 'react'
 import './Login.css'
+
+
 export default function Login() {
+
+  const email = useRef()
+  const password = useRef()
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log('clicked')
+  }
   return (
     <div className='login'>
       <div className="loginWrapper">
@@ -8,9 +19,21 @@ export default function Login() {
             <div className="loginDesc">Connect with friends and family all over the globe on Mksocial</div>
         </div>
         <div className="loginRight">
-            <form className="loginForm">
-                <input placeholder="Email" type='email' className="loginInput" />
-                <input placeholder="Password" type='password' className="loginInput" />
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <input 
+                  placeholder="Email"
+                  required
+                  type='email' 
+                  className="loginInput" 
+                  ref={email}
+                  />
+                <input 
+                  placeholder="Password" 
+                  type='password' 
+                  required
+                  className="loginInput" 
+                  ref={password}
+                />
                 <button className="loginSubmit" type='submit'>Log in</button>
                 <span className="loginForgot">Forgot Password?</span>
                 <button className="loginCreateNew">Create new account</button>
