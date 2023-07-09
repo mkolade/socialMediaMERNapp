6047883240e941fa7d455ab0d05fa1c8c462c2eb
moshoodmohammed <mohammedamoshood@gmail.com>
2023-07-09 16:59:43 +0100

    removed console.log in login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,31 @@
 import HomePage from "./pages/HomePage/HomePage"
 import Login from "./pages/Login/Login"
 import Profile from "./pages/Profile/Profile"
-import {BrowserRouter as Router, Routes,Route } from "react-router-dom"
+import {BrowserRouter as Router, Routes,Route,Navigate } from "react-router-dom"
 import Register from "./pages/Register/Register"
+import { useContext } from "react"
+import { AuthContext } from "./context/AuthContext"
  
 
 function App() {
-
+  const {user} = useContext(AuthContext)
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path={'/'} element={<HomePage/>}/>
-          <Route path={'/profile/:username'} element={<Profile/>}/>
-          <Route path={'/login'} element={<Login/>}/>
-          <Route path={'/register'} element={<Register/>}/>
+          <Route exact path={'/'} element={user ? <HomePage/> : <Register/> }/>
+          <Route 
+            path={'/profile/:username'} 
+            element={<Profile/>}    
+          />
+          <Route 
+            path={'/login'} 
+            element={user ? <Navigate to={'/'} replace/> :<Login/>}
+          />
+          <Route 
+            path={'/register'}  
+            element={user ? <Navigate to={'/'} replace/> :<Register/>}
+          />
         </Routes>
       </div>
     </Router>
